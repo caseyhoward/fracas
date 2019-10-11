4f66408f42c6d1b57d3c3c762e436f91121f0b3b
Casey Howard <caseyhoward@caseyhoward.com>
2019-10-11 10:50:41 -0700

    Move client stuff to client folder

---
+++ client/tests/MapTest.elm
@@ -0,0 +1,23 @@
+module GameTests exposing (..)
+
+import Game
+import Color
+import Dict
+import Expect
+import Set
+import Test exposing (..)
+
+
+
+
+
+-- Check out http://package.elm-lang.org/packages/elm-community/elm-test/latest to learn more about testing in Elm!
+
+
+all : Test
+all =
+    describe "Game"
+        [ test ".start" <|
+            \_ ->
+                Expect.equal True True
+        ]
