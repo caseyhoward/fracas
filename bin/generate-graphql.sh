a41e7714667fee7e09015736dbed3a2a0eee3e30
Casey Howard <caseyhoward@caseyhoward.com>
2019-10-15 20:42:35 -0700

    Don't destory elm graphql files if the new ones fail to generate. This makes developing less annoy since you don't start getting a bunch of elm errors.

--- bin/generate-graphql.sh
+++ bin/generate-graphql.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env sh
 
-rm -rf client/src/Api
-cd client && elm-graphql http://localhost:4000 --base Api && cd -
+
+if [ -d client/src/Api ]; then
+  mv client/src/Api client/src/Api.bak
+fi
+
+cd client && ( (elm-graphql http://localhost:4000 --base Api && rm -rf client/src/Api.bak) || (mv src/Api.bak src/Api && exit 1 ) ) && cd -
 cd server && npm run graphql-codegen && cd -
